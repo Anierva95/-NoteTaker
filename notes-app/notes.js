a780fa90ad19f2cdd7fdc7dc37c0e5b935c5795f
Anierva95 <46223963+Anierva95@users.noreply.github.com>
2020-02-18 09:37:46 -0500

    added duplicate note filter

--- notes-app/notes.js
+++ notes-app/notes.js
@@ -7,12 +7,40 @@ const getNotes = () => {
 
 const addNote = function(title,body) {
     const notes = loadNotes()
+    const duplicateNotes = notes.filter(function(note) {
+        return note.title === title
+    })
+
+    if (diplicateNotes.length === 0) {
+        // PUSH object properties
+        notes.push({
+            title: title,
+            body: body
+    })
+
+    saveNotes(notes);
+
+
+    }
+
+
+}
+
+const saveNotes = function(notes) {
+    dataJSON = JSON.stringify(notes);
+    fs.writeFileSync('notes.json', dataJSON);
 }
 
 const loadNotes = function() {
-    const dataBuffer = fs.readFileSync('notes.json');
-    const dataJSON = dataBuffer.toString();
-    return JSON.parse(dataJSON);
+    try {
+        const dataBuffer = fs.readFileSync('notes.json');
+        const dataJSON = dataBuffer.toString();
+        return JSON.parse(dataJSON); 
+    } catch (e) {
+        return [];
+    }
+
+    
 }
 
 module.exports = {
